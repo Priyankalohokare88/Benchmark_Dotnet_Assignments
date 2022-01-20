58c6653fa6f3f6bbbeda690b9b455e86450992a4
Priyankalohokare88 <priyankalohokare.benchmarkit.solutions>
2022-01-20 22:45:40 +0530

    updated

---
+++ RazorViewEngineAssignment/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewEngineAssignment.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+
+            ViewBag.amt = 90876;
+            return View();
+        }
+    }
+}
---
+++ RazorViewEngineDemo/Controllers/studentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewEngineDemo.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public ActionResult StudentMarks()
+        {
+            ViewBag.StudentId = 1;
+            ViewBag.StudentName = "Priyanka";
+            ViewBag.StudentClass = "Symsc";
+            ViewBag.StudentMark = 50;
+            ViewBag.subject = new List<string>() {"Maths","English","Physics" };
+            return View();
+        }
+    }
+}

790f5f3510168003041cd941109ad0c2fb2c4e66
Priyankalohokare88 <priyankalohokare.benchmarkit.solutions>
2022-01-25 00:19:40 +0530

    updated

---
+++ SharedViewDemo/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SharedViewDemo.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.contactno = "809-809-809";
+            return View();
+        }
+    }
+}
---
+++ SharedViewDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SharedViewDemo.MvcApplication" Language="C#" %>

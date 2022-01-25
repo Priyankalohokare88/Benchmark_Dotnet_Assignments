5e4ffeef4168285cfaa7a9ab35c8fcb52d1a54d3
Priyankalohokare88 <priyankalohokare.benchmarkit.solutions>
2022-01-25 18:48:19 +0530

    updated

---
+++ UrlRoutingDemo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlRoutingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Route("")]
+        [Route("Home/GetId/{pname}")]
+        public ActionResult Index(string pname)
+        {
+            int id1=0;
+            var product = new[]
+            {new {pname="iphone" ,id=1},
+            new {pname="tv" ,id=2},
+            new {pname="camera" ,id=3}};
+            foreach(var item in product)
+            {
+                if(item.pname == pname)
+                {
+                     id1=item.id;
+                }
+            }
+            return Content( id1.ToString());
+        }
+        [Route("Home/GetName/{pid:int?}")]
+        public ActionResult GetName(int pid)
+        {
+            string pname = "null";
+            var product = new[]
+            {new {pname="iphone" ,id=1},
+            new {pname="tv" ,id=2},
+            new {pname="camera" ,id=3}};
+            if (pid == null)
+            {
+                return Content("Please enter product id");
+            }
+            else
+            {
+                foreach (var item in product)
+                {
+                    if (item.id==pid)
+                    {
+                        pname = item.pname;
+                    }
+                }
+
+                return Content(pname);
+            }
+        }
+    }
+}

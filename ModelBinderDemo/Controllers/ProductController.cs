66aa9eb80b023507b8739b479638017d796a0b72
Priyankalohokare88 <priyankalohokare.benchmarkit.solutions>
2022-01-27 20:19:22 +0530

    updated

---
+++ ModelBinderDemo/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBinderDemo.Models;
+
+namespace ModelBinderDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create1([Bind (Include   ="pid,pname")]Product p)
+        {
+            ViewBag.p = p;
+            return View();
+        }
+    }
+}
---
+++ ModelBinderDemo/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBinderDemo.Models
+{
+    public class Product
+    {
+        public int pid
+        { get; set; }
+        public string pname { get; set; }
+        public int price { get; set; }
+
+
+    }
+}

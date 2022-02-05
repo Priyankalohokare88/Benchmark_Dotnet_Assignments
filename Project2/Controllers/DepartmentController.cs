548ae6c559b9f377240109d58671df536a4b6696
Priyankalohokare88 <priyankalohokare.benchmarkit.solutions>
2022-02-05 23:31:37 +0530

    Assignment on CRUD Operations

---
+++ Project2/Controllers/DepartmentController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project2.Models;
+
+namespace Project2.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index(string SortColumn="Depname",string IconClass="fa-sort-asc")
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+            
+List<Department> dep = db.Departments.ToList();
+            ViewBag.SortColumn=SortColumn;
+            ViewBag.IconClass=IconClass;
+            if (ViewBag.SortColumn == "Depid")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    dep = dep.OrderBy(temp => temp.Depid).ToList();
+
+                else
+                    dep = dep.OrderByDescending(temp => temp.Depid).ToList();
+            }
+            else if (ViewBag.SortColumn == "Depname")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    dep = dep.OrderBy(temp => temp.Depname).ToList();
+
+                else
+                    dep = dep.OrderByDescending(temp => temp.Depname).ToList();
+            }
+            if (ViewBag.SortColumn == "DepLocation")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    dep = dep.OrderBy(temp => temp.DepLocation).ToList();
+
+                else
+                    dep = dep.OrderByDescending(temp => temp.DepLocation).ToList();
+            }
+            return View(dep);
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmpDep1Entities db = new EmpDep1Entities();
+            Department departments = db.Departments.Where(temp => temp.Depid == id).FirstOrDefault();
+            return View(departments);
+        }
+        public ActionResult Create()
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department d)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+
+            db.Departments.Add(d);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+      
+            Department exp = db.Departments.Where(temp => temp.Depid == id).FirstOrDefault();
+            return View(exp);
+        }
+        [HttpPost]
+        public ActionResult Edit(Department d)
+        {
+            EmpDep1Entities db = new EmpDep1Entities();
+
+            Department exp = db.Departments.Where(temp => temp.Depid == d.Depid).FirstOrDefault();
+            exp.Depname = d.Depname;
+            exp.DepLocation = d.DepLocation;
+            
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        public ActionResult Delete(int id)
+        {
+            EmpDep1Entities db = new EmpDep1Entities();
+
+            Department exp = db.Departments.Where(temp => temp.Depid == id).FirstOrDefault();
+            db.Departments.Remove(exp);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+        [HttpPost]
+        //we are passing product p dumy bcz we have already mehod delete to make this method diff we pass product p
+        public ActionResult Delete(int id, Department d)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+
+            Department exp = db.Departments.Where(temp => temp.Depid == id).FirstOrDefault();
+            db.Departments.Remove(exp);
+            db.SaveChanges();
+           
+            return RedirectToAction("Index");
+        }
+    //    public ActionResult Search(Department d, string SortColumn = "Depname", string IconClass = "fa-sort-asc")
+    //    {
+
+    //        EmpDep1Entities db = new EmpDep1Entities();
+    //        ViewBag.s = d;
+    //        //List<Department> dep = db.Departments.Where(temp => temp.Depname.Contains(d.Depname)||
+    //        //temp.Depname.Contains(null)&& 
+    //        //temp.DepLocation.Contains(d.DepLocation)).ToList();
+    //        //List<Department> dep = db.Departments.ToList();
+    //        //if (d.Depname != null && d.DepLocation != null&& d.Depid!=0) 
+    //        //{
+    //        //    List<Department> dep = db.Departments.Where(temp => temp.Depname.Contains(d.Depname)
+    //        //    && temp.DepLocation.Contains(d.DepLocation)).ToList();
+    //        //    return View(dep);
+    //        //}
+
+
+    //        //List<Department> dep = db.Departments.WhereIf(temp => temp.Depname.Contains(d.Depname)
+    //        // || temp.DepLocation.Contains(d.DepLocation)).ToList();
+
+
+    //        List<Department> dep = db.Departments
+    //.Where( s => s.Depname == d.Depname||d.Depname==null)
+    //.Where(s => s.DepLocation == d.DepLocation || d.DepLocation == null)
+    //.ToList();
+    //        return View(dep);
+    //        }
+            
+
+
+        }
+
+    }
---
+++ Project2/Controllers/EmployeeController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project2.Models;
+
+namespace Project2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(string SortColumn = "Empname", string IconClass = "fa-sort-asc")
+        {
+
+            EmpDep1Entities db=new EmpDep1Entities();
+            List<employee> emp = db.employees.ToList();
+            ViewBag.SortColumn = SortColumn;
+            ViewBag.IconClass = IconClass;
+            if (ViewBag.SortColumn == "Empid")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Empid).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Empid).ToList();
+            }
+            else if (ViewBag.SortColumn == "Empname")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Empname).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Empname).ToList();
+            }
+            else if (ViewBag.SortColumn == "Designation")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Designation).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Designation).ToList();
+            }
+            else if (ViewBag.SortColumn == "Mgid")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Mgid).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Mgid).ToList();
+            }
+            else if (ViewBag.SortColumn == "Emailid")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Emailid).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Emailid).ToList();
+            }
+           else if (ViewBag.SortColumn == "Hire_date")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Hire_date).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Hire_date).ToList();
+            }
+            else if (ViewBag.SortColumn == "sal")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.sal).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.sal).ToList();
+            }
+            else if (ViewBag.SortColumn == "Depid")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    emp = emp.OrderBy(temp => temp.Depid).ToList();
+
+                else
+                    emp = emp.OrderByDescending(temp => temp.Depid).ToList();
+            }
+            return View(emp);
+
+        
+
+    }
+    public ActionResult Details(int id)
+        {
+            EmpDep1Entities db = new EmpDep1Entities();
+             employee employees = db.employees.Where(temp => temp.Empid == id).FirstOrDefault();
+            return View(employees);
+        }
+        public ActionResult Create()
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+            ViewBag.dep = db.Departments;
+            ViewBag.msg = "New Employee Added Successfully!!";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(employee e)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+
+            db.employees.Add(e);
+            db.SaveChanges();
+            ViewBag.msg = "New Employee Added Successfully!!";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+
+
+            ViewBag.dep = db.Departments;
+          
+            employee exp = db.employees.Where(temp => temp.Empid == id).FirstOrDefault();
+            return View(exp);
+        }
+        [HttpPost]
+        public ActionResult Edit(employee e)
+        {
+            EmpDep1Entities db = new EmpDep1Entities();
+            employee exp = db.employees.Where(temp => temp.Empid == e.Empid).FirstOrDefault();
+            exp.Empname = e.Empname;
+            exp.Designation=e.Designation;
+            exp.Mgid = e.Mgid;
+            exp.Emailid=e.Emailid;
+            exp.Hire_date = e.Hire_date;
+            exp.sal=e.sal;
+            exp.Depid=e.Depid;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        public ActionResult Search(employee e)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+            ViewBag.s = e;
+            List<employee> emp = db.employees
+           .Where(s => s.Empname.Contains(e.Empname) || e.Empname == null)
+           .Where(s => s.Designation.Contains(e.Designation)||e.Designation==null)
+           .Where(s=>s.Mgid== e.Mgid||e.Mgid==null)
+           .Where(s=>s.Depid== e.Depid || e.Depid==null)
+           .Where(s => s.Depid == e.Depid || e.Depid ==null)
+
+
+
+
+           .ToList();
+            ViewBag.dep = db.Departments;
+
+            return View(emp);
+        }
+
+     
+        //we are passing product p dumy bcz we have already mehod delete to make this method diff we pass product p
+        public ActionResult Delete(int id, employee d)
+        {
+
+            EmpDep1Entities db = new EmpDep1Entities();
+
+            employee exp = db.employees.Where(temp => temp.Empid == id).FirstOrDefault();
+            db.employees.Remove(exp);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+            
